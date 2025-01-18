5fb66282b9fe6ad9552a2809c997de86a115f215
Amanj5486 <amanj5486@gmail.com>
2025-01-18 18:05:50 +0530

    change setSchedule as well as pagination in get Requesst

--- infra/src/main/java/com/myorg/InfraApp.java
+++ infra/src/main/java/com/myorg/InfraApp.java
@@ -10,7 +10,7 @@ public class InfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new InfraStack(app, "EC2Stack", StackProps.builder()
+        new InfraStack(app, "InfraStack", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
--- infra/src/main/java/com/myorg/InfraStack.java
+++ infra/src/main/java/com/myorg/InfraStack.java
@@ -1,27 +1,14 @@
 package com.myorg;
 
-import software.amazon.awscdk.CfnOutput;
-import software.amazon.awscdk.Duration;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
-import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
-import software.amazon.awscdk.services.ecs.ScalableTaskCount;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
-import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
-import software.amazon.awscdk.services.iam.PolicyStatement;
-import software.amazon.awscdk.services.iam.Role;
-import software.amazon.awscdk.services.iam.ServicePrincipal;
-import software.amazon.awscdk.services.s3.Bucket;
-import software.amazon.awscdk.services.s3.IBucket;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.apigateway.*;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.s3.*;
 import software.constructs.Construct;
-import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 
 import java.util.List;
-
 
 public class InfraStack extends Stack {
 
@@ -30,98 +17,87 @@ public class InfraStack extends Stack {
     }
 
     public InfraStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+        super(scope, id, StackProps.builder()
+                .env(Environment.builder()
+                        .account("425717139425")   // Replace with your AWS account ID
+                        .region("ap-south-1")        // Replace with your desired AWS region (e.g., us-east-1)
+                        .build())
+                .build());
 
-        // Create a VPC with 2 availability zones and 1 NAT gateway
-        Vpc vpc = Vpc.Builder.create(this, "ExampleSpringbootApplicationVpc")
-                .maxAzs(2)   // Maximum of 2 Availability Zones
-                .natGateways(1)
-                .build();
+        // Use the default VPC to avoid additional NAT Gateway costs
+        IVpc vpc = Vpc.fromLookup(this, "DefaultVpc", VpcLookupOptions.builder().isDefault(true).build());
 
-        // Create an ECS Cluster in the VPC
+        // Create an ECS Cluster in the default VPC
         Cluster cluster = Cluster.Builder.create(this, "ExampleSpringbootApplicationCluster")
                 .vpc(vpc)
                 .clusterName("ApplicationCluster")
                 .build();
 
-        // Define the Fargate service with an Application Load Balancer
-        ApplicationLoadBalancedFargateService fargateService = ApplicationLoadBalancedFargateService.Builder
-                .create(this, "ExampleSpringbootLoadBalancedApplication")
-                .cluster(cluster)              // Link to the ECS Cluster
-                .desiredCount(1)               // Start with 2 tasks
-                .cpu(256)                      // CPU units
-                .memoryLimitMiB(512)           // Memory limit in MiB
-                .taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
-                        .image(ContainerImage.fromAsset("../functions"))  // Local Docker image
-                        .containerPort(8080)      // Spring Boot app port
-                        .build())
+        // Define Fargate Task Definition
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "TaskDef")
+                .cpu(256)                        // Minimal CPU (256 = 0.25 vCPU)
+                .memoryLimitMiB(512)             // Minimal Memory (512 MiB)
                 .build();
-        // Configure health checks for the application
-        fargateService.getTargetGroup().configureHealthCheck(HealthCheck.builder()
-                .port("traffic-port")
-                .path("/actuator/health")         // Spring Boot Actuator health endpoint
-                .interval(Duration.seconds(5))    // Health check interval
-                .timeout(Duration.seconds(4))     // Timeout for health checks
-                .healthyThresholdCount(2)         // Number of successes to mark as healthy
-                .unhealthyThresholdCount(2)       // Number of failures to mark as unhealthy
-                .healthyHttpCodes("200,301,302")
-                .build()); // Acceptable HTTP response codes
 
-        EnableScalingProps enableScalingProps = EnableScalingProps.builder().maxCapacity(4).minCapacity(2).build();
-
-        // Auto Scaling based on CPU Utilization
-        ScalableTaskCount scaling = fargateService.getService().autoScaleTaskCount(enableScalingProps);
-
-        scaling.scaleOnCpuUtilization("CpuAutoScaling", CpuUtilizationScalingProps.builder()
-                .targetUtilizationPercent(45)       // Scale when CPU utilization exceeds 45%
-                .scaleInCooldown(Duration.seconds(30))
-                .scaleOutCooldown(Duration.seconds(30))
+        // Add container to the task definition
+        taskDefinition.addContainer("SpringBootApp", ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromAsset("../functions"))  // Local Docker image
+                .portMappings(List.of(
+                        PortMapping.builder()
+                                .containerPort(8080)
+                                .build()))
                 .build());
 
         // S3 Buckets
-        String existingBucketName = "patient-image-1";
-        IBucket existingBucket = Bucket.fromBucketName(this, "ExistingBucket", existingBucketName);
+        IBucket existingBucket = Bucket.fromBucketName(this, "ExistingBucket", "patient-image-1");
+        IBucket existingBucket1 = Bucket.fromBucketName(this, "ExistingBucket1", "doctor-image-1");
 
-        String existingBucketName_1 = "doctor-image-1";
-        IBucket existingBucket_1 = Bucket.fromBucketName(this, "ExistingBucket_1", existingBucketName_1);
-
-        // Create an IAM role with read permissions for the S3 buckets
+        // IAM Role for ECS Task
         Role role = Role.Builder.create(this, "EcsTaskExecutionRole")
                 .assumedBy(new ServicePrincipal("ecs-tasks.amazonaws.com"))
                 .build();
 
-        // Add read permissions for the existing buckets
         role.addToPolicy(PolicyStatement.Builder.create()
-                .actions(List.of("s3:GetObject", "s3:ListBucket"))
+                .actions(List.of("s3:GetObject", "s3:ListBucket", "s3:PutObject"))
                 .resources(List.of(
-                        existingBucket.getBucketArn(), // Bucket ARN
-                        existingBucket.getBucketArn() + "/*" // Bucket objects ARN
+                        existingBucket.getBucketArn(), existingBucket.getBucketArn() + "/*",
+                        existingBucket1.getBucketArn(), existingBucket1.getBucketArn() + "/*"
                 ))
                 .build());
 
-        role.addToPolicy(PolicyStatement.Builder.create()
-                .actions(List.of("s3:GetObject", "s3:ListBucket","s3:PutObject"))
+        // Attach IAM role to the Task Definition
+        taskDefinition.addToExecutionRolePolicy(PolicyStatement.Builder.create()
+                .actions(List.of("s3:GetObject", "s3:ListBucket", "s3:PutObject"))
                 .resources(List.of(
-                        // Bucket objects ARN
-                        existingBucket_1.getBucketArn(), // Bucket ARN
-                        existingBucket_1.getBucketArn() + "/*" // Bucket objects ARN
+                        existingBucket.getBucketArn(), existingBucket.getBucketArn() + "/*",
+                        existingBucket1.getBucketArn(), existingBucket1.getBucketArn() + "/*"
                 ))
                 .build());
 
-        // Attach the role to the Fargate task definition
-        fargateService.getTaskDefinition().addToExecutionRolePolicy(PolicyStatement.Builder.create()
-                .actions(List.of("s3:GetObject", "s3:ListBucket"))
-                .resources(List.of(
-                        existingBucket.getBucketArn(), // Bucket ARN
-                        existingBucket.getBucketArn() + "/*", // Bucket objects ARN
-                        existingBucket_1.getBucketArn(), // Bucket ARN
-                        existingBucket_1.getBucketArn() + "/*" // Bucket objects ARN
-                ))
-                .build());
-
-        CfnOutput.Builder.create(this, "LoadBalancerURL")
-                .value(fargateService.getLoadBalancer().getLoadBalancerDnsName())
-                .description("Public URL of the Load Balancer")
+        // Fargate Service without Load Balancer
+        FargateService fargateService = FargateService.Builder.create(this, "FargateService")
+                .cluster(cluster)
+                .taskDefinition(taskDefinition)
+                .desiredCount(1)   // Minimal number of tasks
                 .build();
+
+        // Use the existing REST API
+        IRestApi api = RestApi.fromRestApiAttributes(this, "ExistingRestAPI",
+                RestApiAttributes.builder()
+                        .restApiId("0ye4ck5ap6")  // Replace with your actual API ID
+                        .rootResourceId("q8yjjsiph5")  // Replace with the root resource ID (usually "/")
+                        .build()
+        );  // Replace "existing-api-id" with your API ID
+
+        // Create resources for your REST API
+        var posts = api.getRoot().addResource("appointment");
+
+
+        // Lambda integration with ECS service (you can use an HTTP Proxy Integration or Lambda function here)
+        posts.addMethod("POST", new HttpIntegration(fargateService.getServiceArn()));
+        posts.addMethod("GET", new HttpIntegration(fargateService.getServiceArn()));
+        posts.addMethod("PUT", new HttpIntegration(fargateService.getServiceArn()));
+
+
     }
 }
