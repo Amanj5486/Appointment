40f563d1cd19d1d974879bb3c93c1d2bb3b4ae80
Amanj5486 <amanj5486@gmail.com>
2025-01-19 21:42:07 +0530

    add page size etc..

--- infra/src/main/java/com/myorg/InfraStack.java
+++ infra/src/main/java/com/myorg/InfraStack.java
@@ -2,11 +2,14 @@ package com.myorg;
 
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.apigateway.*;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.iam.*;
 import software.amazon.awscdk.services.s3.*;
 import software.constructs.Construct;
+
+
 
 import java.util.List;
 
@@ -27,16 +30,35 @@ public class InfraStack extends Stack {
         // Use the default VPC to avoid additional NAT Gateway costs
         IVpc vpc = Vpc.fromLookup(this, "DefaultVpc", VpcLookupOptions.builder().isDefault(true).build());
 
-        // Create an ECS Cluster in the default VPC
+        // Create an ECS Cluster in the default VPC with EC2 instances
         Cluster cluster = Cluster.Builder.create(this, "ExampleSpringbootApplicationCluster")
                 .vpc(vpc)
                 .clusterName("ApplicationCluster")
                 .build();
 
-        // Define Fargate Task Definition
-        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "TaskDef")
-                .cpu(256)                        // Minimal CPU (256 = 0.25 vCPU)
-                .memoryLimitMiB(512)             // Minimal Memory (512 MiB)
+        // Define EC2 Auto Scaling Group for ECS instances
+        InstanceType instanceType = InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO);  // t2.micro or t3.micro for free tier
+        AutoScalingGroup autoScalingGroup = AutoScalingGroup.Builder.create(this, "EcsAutoScalingGroup")
+                .vpc(vpc)  // Attach to existing VPC
+                .instanceType(instanceType)  // Specify instance type for EC2
+                .minCapacity(1)  // Minimum number of EC2 instances (1)
+                .maxCapacity(1)
+                .machineImage(MachineImage.latestAmazonLinux())// Maximum number of EC2 instances (2)
+                .build();
+        AsgCapacityProvider asgCapacityProvider = new AsgCapacityProvider(this, "AsgCapacityProvider",
+                AsgCapacityProviderProps.builder()
+                        .autoScalingGroup(autoScalingGroup)
+                        .build());
+
+        // Attach Auto Scaling Group as Capacity Provider to ECS Cluster
+        cluster.addAsgCapacityProvider(asgCapacityProvider);
+
+
+        // Define ECS Task Definition for EC2
+        TaskDefinition taskDefinition = TaskDefinition.Builder.create(this, "TaskDef")
+                .compatibility(Compatibility.EC2)  // Specify EC2 compatibility instead of Fargate
+                .cpu("256")  // Minimal CPU (256 = 0.25 vCPU)
+                .memoryMiB("512")  // Minimal Memory (512 MiB)
                 .build();
 
         // Add container to the task definition
@@ -74,11 +96,11 @@ public class InfraStack extends Stack {
                 ))
                 .build());
 
-        // Fargate Service without Load Balancer
-        FargateService fargateService = FargateService.Builder.create(this, "FargateService")
+        // ECS Service on EC2 Instances
+        Ec2Service ec2Service = Ec2Service.Builder.create(this, "Ec2Service")
                 .cluster(cluster)
                 .taskDefinition(taskDefinition)
-                .desiredCount(1)   // Minimal number of tasks
+                .desiredCount(1) // Only one task to minimize cost
                 .build();
 
         // Use the existing REST API
@@ -92,12 +114,9 @@ public class InfraStack extends Stack {
         // Create resources for your REST API
         var posts = api.getRoot().addResource("appointment");
 
-
         // Lambda integration with ECS service (you can use an HTTP Proxy Integration or Lambda function here)
-        posts.addMethod("POST", new HttpIntegration(fargateService.getServiceArn()));
-        posts.addMethod("GET", new HttpIntegration(fargateService.getServiceArn()));
-        posts.addMethod("PUT", new HttpIntegration(fargateService.getServiceArn()));
-
-
+        posts.addMethod("POST", new HttpIntegration(ec2Service.getServiceArn()));
+        posts.addMethod("GET", new HttpIntegration(ec2Service.getServiceArn()));
+        posts.addMethod("PUT", new HttpIntegration(ec2Service.getServiceArn()));
     }
 }
